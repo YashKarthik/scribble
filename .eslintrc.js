79d369801d2ecb77a383ba115d10ba219ce90e7b
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-03-14 18:22:16 +0530

    mess around with eslint; should fix later

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  //overrides: [
+  //  {
+  //    extends: [
+  //      "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  //    ],
+  //    files: ["*.ts", "*.tsx"],
+  //    parserOptions: {
+  //      project: "tsconfig.json",
+  //    },
+  //  },
+  //],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/no-misused-promises": "warn",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ]
+  }
+};
+
+module.exports = config;
